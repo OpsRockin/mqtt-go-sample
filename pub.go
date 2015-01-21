6587bf5dfc2d6ab97eee8fdbf6aee4bf70a9cdfb
azukiwasher <azukiwasher@higanworks.com>
2015-01-21 18:20:32 +0900

    Load config in TOML format

--- pub.go
+++ pub.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"io"
 	"os"
 	"strconv"
@@ -13,16 +14,33 @@ import (
 
 import MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
 
+type Config struct {
+	Host     string `toml:"Host"`
+	Port     int    `toml:"Port"`
+	Topic    string `toml:"Topic"`
+	QoS      int    `toml:"QoS"`
+	Username string `toml:"Username"`
+	Password string `toml:"Password"`
+}
+
 func main() {
+	var config Config
+	var err error
+
+	_, err = toml.DecodeFile("config.tml", &config)
+	if err != nil {
+		panic(err)
+	}
+
 	stdin := bufio.NewReader(os.Stdin)
 	hostname, _ := os.Hostname()
 
-	server := flag.String("server", "tcp://127.0.0.1:1883", "The endpoint of the MQTT")
+	server := flag.String("server", config.Host+":"+strconv.Itoa(config.Port), "The endpoint of the MQTT")
 	topic := flag.String("topic", hostname, "The topic to publish the messages on")
-	qos := flag.Int("qos", 0, "The QoS to send the messages at")
+	qos := flag.Int("qos", config.QoS, "The QoS to send the messages at")
 	client_id := flag.String("client_id", hostname+strconv.Itoa(time.Now().Second()), "A client ID for the connection")
-	username := flag.String("username", "", "A username to authenticate to the MQTT server")
-	password := flag.String("password", "", "Password to match username")
+	username := flag.String("username", config.Username, "A username to authenticate to the MQTT server")
+	password := flag.String("password", config.Password, "Password to match username")
 	flag.Parse()
 
 	connOpts := MQTT.NewClientOptions().AddBroker(*server).SetClientId(*client_id).SetCleanSession(true)
@@ -34,7 +52,7 @@ func main() {
 	}
 
 	client := MQTT.NewClient(connOpts)
-	_, err := client.Start()
+	_, err = client.Start()
 	if err != nil {
 		panic(err)
 	} else {
@@ -48,6 +66,6 @@ func main() {
 		}
 		r := client.Publish(MQTT.QoS(*qos), *topic, []byte(strings.TrimSpace(message)))
 		<-r
-		//fmt.Print("Message Sent")
+		fmt.Print("Message Sent\n")
 	}
 }
--- sub.go
+++ sub.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"os"
 	"os/signal"
 	"strconv"
@@ -12,14 +13,32 @@ import (
 
 import MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
 
+type Config struct {
+	Host     string `toml:"Host"`
+	Port     int    `toml:"Port"`
+	Topic    string `toml:"Topic"`
+	QoS      int    `toml:"QoS"`
+	Username string `toml:"Username"`
+	Password string `toml:"Password"`
+}
+
 func onMessageReceived(client *MQTT.MqttClient, message MQTT.Message) {
 	fmt.Printf("Received message on topic: %s\n", message.Topic())
 	fmt.Printf("Message: %s\n", message.Payload())
 }
 
 func main() {
+	var config Config
+	var err error
+
+	_, err = toml.DecodeFile("config.tml", &config)
+	if err != nil {
+		panic(err)
+	}
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
 	go func() {
 		<-c
 		fmt.Println("signal received, existing")
@@ -28,12 +47,12 @@ func main() {
 
 	hostname, _ := os.Hostname()
 
-	server := flag.String("server", "tcp://127.0.0.1:1883", "The endpoint of the MQTT")
+	server := flag.String("server", config.Host+":"+strconv.Itoa(config.Port), "The endpoint of the MQTT")
 	topic := flag.String("topic", hostname, "The topic to publish the messages on")
-	qos := flag.Int("qos", 0, "The QoS to send the messages at")
+	qos := flag.Int("qos", config.QoS, "The QoS to send the messages at")
 	client_id := flag.String("client_id", hostname+strconv.Itoa(time.Now().Second()), "A client ID for the connection")
-	username := flag.String("username", "", "A username to authenticate to the MQTT server")
-	password := flag.String("password", "", "Password to match username")
+	username := flag.String("username", config.Username, "A username to authenticate to the MQTT server")
+	password := flag.String("password", config.Password, "Password to match username")
 	flag.Parse()
 
 	connOpts := MQTT.NewClientOptions().AddBroker(*server).SetClientId(*client_id).SetCleanSession(true)
@@ -45,7 +64,8 @@ func main() {
 	}
 
 	client := MQTT.NewClient(connOpts)
-	_, err := client.Start()
+
+	_, err = client.Start()
 	if err != nil {
 		panic(err)
 	} else {
